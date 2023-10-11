51f62aea3040d140354b231221e00bc30fc309b9
Timur <tim.vildanov@gmail.com>
2023-10-11 22:40:10 +0500

    parser from file

---
+++ gendiff/__init__.py
@@ -0,0 +1,3 @@
+from gendiff.tree_engine import generate_diff
+
+__all__ = ('generate_diff',)
--- gendiff/tree_engine.py
+++ gendiff/tree_engine.py
@@ -2,5 +2,7 @@
 def build_diff(initial_dict, changed_dict):
     """Function creates a difference between two dictionaries"""
     diff_dict = {}
+    pass
 
-
+def generate_diff(first_file, second_file, format = 'string'):
+    pass
